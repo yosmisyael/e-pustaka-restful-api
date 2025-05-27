bfc7e542b867d514f265daa476bbf5edccfe9be3
yosmisyael <yosmisyael7308@gmail.com>
2025-05-27 08:11:59 +0700

    feat: implement cors middleware

--- src/application/web.ts
+++ src/application/web.ts
@@ -3,8 +3,17 @@ import {publicRouter} from "../route/public-api";
 import {errorMiddleware} from "../middleware/error-middleware";
 import {authRouter} from "../route/auth-api";
 import {adminRouter} from "../route/admin-api";
+import cors from "cors";
 
 export const web = express();
+
+web.use(cors({
+    origin: "*",
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    optionsSuccessStatus: 204
+}));
 
 web.use(express.json());
 
