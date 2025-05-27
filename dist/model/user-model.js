7c37948c15dfdd65777a94c0da6822ad6f97db2c
yosmisyael <yosmisyael7308@gmail.com>
2025-05-27 08:07:20 +0700

    build: added cors dep

--- dist/model/user-model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toUserResponse = toUserResponse;
-function toUserResponse(user) {
-    return {
-        email: user.email,
-        name: user.name,
-    };
-}
